4a089fc5ab399431c3e4398aedbc07b572a85901
null <>
2020-02-03 11:11:02 +0100

    Update explo.c

--- explo.c
+++ explo.c
@@ -11,20 +11,20 @@ struct lieu {
 };
 typedef struct lieu ville;
 
-void deplacement (const char * a,const char * b,const char * c,const char * choixVille,int * postionJoueur){	
+void deplacement (const char * a,const char * b,const char * c,const char * choixVille,int * postionJoueur,ville un,ville deux, ville trois){	
 	if (strcmp(choixVille,a)==0){* postionJoueur = 0;}
 	else if (strcmp(choixVille,b)==0){* postionJoueur = 1;}
 	else if (strcmp(choixVille,c)==0){* postionJoueur = 2;}
-	if (* postionJoueur == 0){printf("Bonjour ! Vous etes a Midgar\n");}
-	else if (* postionJoueur == 1){printf("Bonjour ! Vous etes a Johto\n");}
-	else if (* postionJoueur == 2){printf("Bonjour ! Vous etes a Astrub\n");}
+	if (* postionJoueur == 0){printf("Bonjour ! Vous etes a Midgar\n");printf("%s \n",un.description);}
+	else if (* postionJoueur == 1){printf("Bonjour ! Vous etes a Johto\n");printf("%s \n",deux.description);}
+	else if (* postionJoueur == 2){printf("Bonjour ! Vous etes a Astrub\n");printf("%s \n",trois.description);}
 };
 
 
 int main(){
 	int postionJoueur = -1; //chaque ville a son numero, 
-	ville midgar = {"midgar",0, "Midgar est la ville principale et le siège du pouvoir de la Shinra",10,"Johto"};
-	ville johto = {"johto",1, "Johto est une région de bord de mer entourée par des chaînes de montagne",1,"Astrub, Midgar"};
+	ville midgar = {"midgar",0, "Midgar est la ville principale et le siege du pouvoir de la Shinra",10,"Johto"};
+	ville johto = {"johto",1, "Johto est une région de bord de mer entouree par des chaînes de montagne",1,"Astrub, Midgar"};
 	ville astrub = {"astrub",2, "Ville centrale dans le monde des douze pour les jeunes ames.",3,"Johto"};
 	char ville1[10] = "midgar";
 	char ville2[10] = "johto";
@@ -33,7 +33,7 @@ int main(){
 
 printf("Vous pouvez choisir un lieu comme destination.Midgar, Johto ou Astrub.\n"); //phrase de demande de destination
 scanf("%s",&choixVille);
-deplacement(ville1,ville2,ville3,choixVille,&postionJoueur);
+deplacement(ville1,ville2,ville3,choixVille,&postionJoueur,midgar,johto, astrub);
 printf("%d \n",postionJoueur);
 
 
